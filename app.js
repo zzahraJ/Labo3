060fb5a7385144a5e719556b7ca931e2e47eb5fc
zzahraJ <r0883814@student.thomasmore.be>
2023-10-16 10:07:05 +0200

    feat: add the meal API

--- app.js
+++ app.js
@@ -1,43 +1,66 @@
-class App{
-    constructor(){
-        this.getLocation();
-        this.lat;
-        this.lng;
-    }
-    getLocation(){
-        navigator.geolocation.getCurrentPosition(
-            this.gotLocation.bind(this),
-            this.errorLocation.bind(this)
-        );
-    }
+class App {
+  constructor() {
+    this.getLocation();
+    this.lat;
+    this.lng;
+  }
 
-    gotLocation(result){
-       // console.log(result);
-       this.lat = result.coords.latitude;
-       this.lng = result.coords.longitude;
-       //console.log(this.lat);
-       this.getWeather();
-    }
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      this.gotLocation.bind(this),
+      this.errorLocation.bind(this)
+    );
+  }
 
-    getWeather(){
-        //84dc183942414da2b9ccf4c869724e4f
-        let url = `https://api.weatherbit.io/v2.0/current?lat=${this.lat}&lon=${this.lng}&key=84dc183942414da2b9ccf4c869724e4f&include=minutely?units=si`;
-        fetch(url).then(response => {
-            //console.log(response);
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            document.querySelector('#weather').innerHTML = data.data[0].app_temp;
-        })
-        .catch(err => {
-            console.log(err);
-        });     
-    }
+  gotLocation(result) {
+    this.lat = result.coords.latitude;
+    this.lng = result.coords.longitude;
+    this.getWeather();
+  }
 
-    errorLocation(err){
+  getWeather() {
+    let url = `https://api.weatherbit.io/v2.0/current?lat=${this.lat}&lon=${this.lng}&key=84dc183942414da2b9ccf4c869724e4f&include=minutely?units=si`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let temp = data.data[0].app_temp;
+        this.getMeal(temp);
+      })
+      .catch((err) => {
         console.log(err);
-    }   
+      });
+  }
+
+  errorLocation(err) {
+    console.log(err);
+  }
+
+  getMeal(temperature) {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((response) => response.json())
+      .then((data) => {
+        let meal = data.meals[0].strMealThumb 
+        this.updateAd(temperature, meal);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  showError(error) {
+    console.log(error);
+  }
+
+  updateAd(temperature, meal) {
+    let text = document.querySelector("h1");
+    let imgMeal = document.querySelector("img");
+    let temp = temperature;
+    let mealImg = meal;
+    text.innerHTML = "Deze maaltijd is perfect, want het is" + temp + "°C";
+    imgMeal.src = mealImg;
+    imgMeal.style.width = "500px";
+    imgMeal.style.height = "500px";
+  }
 }
 
 let app = new App();
